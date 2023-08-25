e5f1a8a22bd6a5abe7568f9aafba3c8396d89f6b
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-08-25 12:54:40 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Peso a;
+            a = new Peso();
+
+            Console.Write("peso: ");
+            a.setpeso(double.Parse(Console.ReadLine()));
+             
+            Console.Write("altura: ");
+            a.setaltura(double.Parse(Console.ReadLine()));
+
+            a.calcular();
+                 
+           
+        }
+    }
+}
